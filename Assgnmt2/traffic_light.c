92f78ead763f52e5f8bbc8eead573175352d1e8b
pradeep <pradeep.ranjan7837@gmail.com>
2024-01-28 07:39:29 -0500

    started with hwassignmentDraft 2

--- Assgnmt2/traffic_light.c
+++ Assgnmt2/traffic_light.c
@@ -1,21 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 
-void simulationOfLight(){
-	printf("Green Light\n");
-    sleep(30); 
+void operatePathALights(int greenDurationSec) {
+    printf("Path A Green Light\n");
+    sleep(greenDurationSec);
 
-    printf("Yellow Light\n");
+    printf("Path A Yellow Light\n");
+    sleep(5);
+
+    printf("Path A Red Light\n");
     sleep(5); 
-
-    printf("Red Light\n");
-    sleep(35); 
 }
 
+void operatePathBLights(int greenDurationSec) {
+    printf("Path B Red Light\n");
+    sleep(5); // Wait for Path A's green and yellow lights
 
-int main(){
-	
-	while(1){
-		simulationOfLight();
-	}
+    printf("Path B Green Light\n");
+    sleep(greenDurationSec);
+
+    printf("Path B Yellow Light\n");
+    sleep(5);
+}
+
+int main() {
+    float greenDurationMin;
+    printf("Enter the duration of the Green Light (in minutes, can be fractional): ");
+    scanf("%f", &greenDurationMin);
+
+    int greenDurationSec = (int)(greenDurationMin * 60);
+
+    while(1) {
+        operatePathALights(greenDurationSec);
+        operatePathBLights(greenDurationSec);
+    }
+    return 0;
 }
