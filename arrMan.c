23c0b09d16c1bee78dd8bf833f8d02630b21e969
pradeep <pradeep.ranjan7837@gmail.com>
2024-01-28 04:29:00 -0500

    updated with race condition: outcome is different each time

--- arrMan.c
+++ arrMan.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdbool.h>
+#include<pthread.h>
+
 
 #define MAX_SIZE 100
 
@@ -7,64 +9,73 @@ typedef struct{
   int arr[MAX_SIZE];
   int size;
 }Array;
+Array array;
+pthread_t insertThread, deleteThread, searchThread;
 
-void insert(Array* array, int val){
- if(array->size < MAX_SIZE){
-  array->arr[array->size] = val;
-  array->size++;
-  printf("\nA new Element %d is added\n", val);
+void* insert(void* arg){
+	int val = *(int*)arg;
+ if(array.size < MAX_SIZE){
+  array.arr[array.size] = val;
+  array.size++;
+  printf("\nFor Insert:-   new Element %d is added\n", val);
  }
  else{
   printf("Array size is maxout so can not be inserted\n");
 
  }
+ return NULL;
 }
 
 
 //delete a element from the array
 
-void delete(Array* array, int val){
+void* delete(void* arg){
+ int val = *(int*)arg;
  bool found = false;
- for(int i = 0; i < array->size; i++){
+ for(int i = 0; i < array.size; i++){
 
-	if(val == array->arr[i]){
-		for(int j = i; j < array->size - 1; j++){
-			array->arr[j] = array->arr[j + 1];
+	if(val == array.arr[i]){
+		for(int j = i; j < array.size - 1; j++){
+			array.arr[j] = array.arr[j + 1];
 		}
 	
-	array->size--;
+	array.size--;
 	found = true;
 	}
  }
-
+	
+	printf("\nUnder Delete -");
 	if(found == true){
-		printf("\nWe have successfully deleled the element:%d \n",val);
+
+		printf("We have successfully deleled the element:%d \n",val);
 	}
 	else{
-		printf("We can not found any item: %d\n", val);
+		printf("We can not founda and delete item: %d\n", val);
 	} 
-
+ 	return NULL;
  
 }
 
-void search(Array* array, int val){
+void* search(void* arg){
+	int val = *(int*)arg;
 	bool found = false;
-	for(int i = 0; i < array->size; i++){
-		if(array->arr[i] == val){
+	for(int i = 0; i < array.size; i++){
+		if(array.arr[i] == val){
 			found = true;
 		}
 	}
-
+	printf("\nFor Search");
 	if(found) {
 		printf("We have a element %d is our array\n", val);
 	}
 	else{
 		printf("we have not found our element in array\n");
 	}
-
+	return NULL;
 
 }
 
+/*
 void display(Array* array){
  printf("The array's Data is:\n");
  for(int i = 0; i < array->size; i++){
@@ -74,24 +85,21 @@ void display(Array* array){
  return;
 
 }
+*/
 
 int main(){
- Array array;
  array.size = 0;
+	int inserVal = 10;
+	int searchVal = 40;
+	int deleteVal = 10;
+	inserVal = 40;
+	pthread_create(&insertThread, NULL, insert, &inserVal);
+	pthread_create(&searchThread, NULL, search, &searchVal);
+ 	pthread_create(&deleteThread, NULL, delete, &deleteVal);
 
- insert(&array, 10);
- insert(&array, 20);
- insert(&array, 7);
- 
- display(&array);
 
- delete(&array, 10);
- delete(&array, 5);
+	pthread_join(insertThread, NULL);
+	pthread_join(deleteThread, NULL);
 
- display(&array);
- insert(&array, 16);
- search(&array, 7);
- search(&array, 16);
- search(&array, 100);
  return 0;
 }
